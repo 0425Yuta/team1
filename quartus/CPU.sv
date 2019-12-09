884feab04ad9540dbcd24d27d2191889a53f6fcc
namachan10777 <namachan10777@gmail.com>
2019-12-10 00:09:14 +0900

    add RAM module

--- quartus/CPU.sv
+++ quartus/CPU.sv
@@ -11,7 +11,18 @@ module CPU(
 	reg LED[9:0] = '{ 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0 };
 	assign LEDR[9:0] = LED[9:0];
 
+	wire address_cpu, address_vga, q_cpu, q_vga, wren_cpu, wren_vga, data_cpu, data_vga;
+
 	PLL pll(.inclk0(CLK1_50), .c0(clock_cpu), .c1(clock_vga));
+	RAM ram(
+		.address_a(address_cpu),
+		.wren_a(wren_cpu),
+		.q_a(q_cpu),
+		.data_a(data_vga),
+		.address_b(address_vga),
+		.wren_b(wren_vga),
+		.q_b(q_vga),
+		.data_b(data_vga));
 
 	always @(posedge clock_cpu) begin
 		LED[0] <= ~LED[0];
