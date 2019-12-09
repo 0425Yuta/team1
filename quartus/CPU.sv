8079854984791c4ee51c1f0f3e9226f5ba5786d3
namachan10777 <namachan10777@gmail.com>
2019-12-09 23:36:24 +0900

    test PLL

--- quartus/CPU.sv
+++ quartus/CPU.sv
@@ -1,17 +1,25 @@
 `default_nettype none
 
 module CPU(
-	output logic LEDR[9:0],
-	input  logic SW[9:0],
+	output wire LEDR[9:0],
+	input  wire SW[9:0],
 	input  wire CLK1_50
 );
-
-	assign LEDR[0] = SW[0];
-	
 	wire clock_cpu;
 	wire clock_vga;
-	
-	PLL pll(.inclk0(CLK1_50), .c0(clock_cpu), .c1(clock_vga)); 
+
+	reg LED[9:0] = '{ 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0 };
+	assign LEDR[9:0] = LED[9:0];
+
+	PLL pll(.inclk0(CLK1_50), .c0(clock_cpu), .c1(clock_vga));
+
+	always @(posedge clock_cpu) begin
+		LED[0] <= ~LED[0];
+	end
+
+	always @(posedge clock_vga) begin
+		LED[1] <= ~LED[1];
+	end
 
 endmodule
 
