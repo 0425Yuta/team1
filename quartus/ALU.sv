a5a5b831a762b394d2e73aa065a7360750f5da30
Nakano Masaki <namachan10777@gmail.com>
2020-01-06 15:40:38 +0900

    add module ALU

---
+++ quartus/ALU.sv
@@ -0,0 +1,9 @@
+module ALU(
+	input wire clock,
+	input wire[15:0] address_ram,
+	input wire[15:0] q_ram,
+	input wire[15:0] data_ram,
+	input wire[15:0] address_rom,
+	input wire[15:0] q_rom
+);
+endmodule
