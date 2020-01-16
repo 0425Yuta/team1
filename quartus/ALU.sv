93b521ee69a126585e9e114dd2b6ef89f4a0460a
Nakano Masaki <namachan10777@gmail.com>
2020-01-17 00:06:24 +0900

    add ram interface in ALU

--- quartus/ALU.sv
+++ quartus/ALU.sv
@@ -2,6 +2,7 @@ module ALU(
 	input  wire clock,
 	output wire[15:0] address_ram,
 	input  wire[15:0] q_ram,
+	output wire wren_ram,
 	output wire[15:0] data_ram,
 	output wire[15:0] address_rom,
 	input  wire[15:0] q_rom,
@@ -14,19 +15,27 @@ module ALU(
 
 reg[15:0] pc = 16'b0;
 assign address_rom = pc;
+reg[15:0] addr = 16'h0000;
+assign address_ram = addr;
+reg[15:0] data = 16'h0000;
+assign data_ram = data;
+reg[15:0] sp = 16'h0000;
+reg wren = 0;
+assign wren_ram = wren;
 
 enum bit[15:0] {
 	NOP = 16'h18,
-	JMP = 16'h06
+	JMP = 16'h06,
+	IMM = 16'h01
 } OPCODE;
 
-enum {
-	ERROR,
+enum bit[15:0] {
 	INIT,
+	WAIT_FETCHING,
 	FETCH,
 	READY,
-	WAIT_FETCHING,
-	APPLY_PC
+	FETCH_OPERAND,
+	ERROR = 16'hffff
 } STATE;
 
 reg[15:0] opcode = NOP;
@@ -54,12 +63,16 @@ always_ff @( posedge clock ) begin
 	if ( state == INIT ) begin
 		pc <= 16'b0;
 		state <= FETCH;
+		wren <= 0;
 	end
 	else if ( state == WAIT_FETCHING) begin
+		pc <= pc + 16'b1;
 		state <= FETCH;
+		wren <= 0;
 	end
 	else if ( state == FETCH ) begin
 		state <= READY;
+		wren <= 0;
 	end
 	else begin
 		if ( state == READY ) begin
@@ -68,18 +81,29 @@ always_ff @( posedge clock ) begin
 		case ( opcode )
 			NOP: begin
 				if ( state == READY ) begin
-					opcode <= q_rom;
-					pc <= pc + 16'b1;
 					state <= WAIT_FETCHING;
 				end
 			end
 			JMP: begin
 				if ( state == READY ) begin
-					state <= APPLY_PC;
+					state <= FETCH_OPERAND;
 				end
-				else if ( state == APPLY_PC ) begin
+				else if ( state == FETCH_OPERAND ) begin
 					pc <= q_rom;
 					state <= WAIT_FETCHING;
+				end
+			end
+			IMM: begin
+				if ( state == READY ) begin
+					state <= FETCH_OPERAND;
+					addr <= sp;
+					sp <= sp + 16'b1;
+				end
+				else if ( state == FETCH_OPERAND ) begin
+					state <= FETCH;
+					pc <= pc + 16'b1;
+					data <= q_rom;
+					wren <= 1;
 				end
 			end
 			default: begin
